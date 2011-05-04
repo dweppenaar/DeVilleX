96a0e00654d81a3ea16ea2633e9ec5b1b75d3a46
De Ville Weppenaar <dweppenaar@live.co.za>
2011-05-04 21:55:36 +0200

    First implementation of Hello World.

---
+++ src/com/devillex/example/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.devillex.example.helloworld;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
